e97bdd34ddd45149e5ceb42b3ac5c914d00ea209
turbek <turbek93@gmail.com>
2017-03-20 19:06:42 +0100

    Add addDestination method

--- src/main/java/DestinationOrganizer.java
+++ src/main/java/DestinationOrganizer.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
 
 public class DestinationOrganizer {
-    private ArrayList<Destination> OrganizedDestinations = new ArrayList<Destination>();
+    private ArrayList<Destination> OrganizedDestinations = new ArrayList<>();
 
     public ArrayList<Destination> organize(Destination ... destinations){
 
         for(Destination destination : destinations){
-            OrganizedDestinations.add(destination);
+            addDestination(destination);
         }
 
         return OrganizedDestinations;
+    }
+
+    private void addDestination(Destination destination) {
+        if (destination.getDependency() == null || destination.getDependency().isVisited()) {
+            OrganizedDestinations.add(destination);
+        }
     }
 
 
--- src/test/java/TestDestinationOrganizer.java
+++ src/test/java/TestDestinationOrganizer.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -5,25 +6,37 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by turbek on 20.03.17.
- */
 public class TestDestinationOrganizer {
 
     private DestinationOrganizer destinationOrganizer;
     private ArrayList<Destination> returnList;
+    private Destination x;
 
     @Before
     public void setUp() throws Exception {
         destinationOrganizer = new DestinationOrganizer();
         returnList = new ArrayList<>();
+        x = new Destination();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        destinationOrganizer = null;
+        returnList = null;
+        x = null;
     }
 
     @Test
-    public void testIfReturnsParameters() throws Exception {
-        Destination x = new Destination();
+    public void testIfReturnsOneParameter() {
         returnList.add(x);
         assertEquals(returnList, destinationOrganizer.organize(x));
     }
 
+    @Test
+    public void testIfDependencyAlreadyVisited(){
+        Destination y = new Destination();
+        y.setVisited(false);
+        x.setDependency(y);
+        assertEquals(returnList, destinationOrganizer.organize(x));
+    }
 }
